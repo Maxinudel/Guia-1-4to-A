32b5f233f67ff08650b09d565322e227783b446d
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2025-04-08 11:28:27 -0300

    Conversion de monedas listo

---
+++ Assets/Scripts/ConversionDeMonedas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeMonedas : MonoBehaviour
+{
+
+    public int Cantidad;
+    public string Moneda;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Cantidad < 1000)
+        {
+            Debug.Log("El monto mínimo es $1000");
+        }else if (Moneda=="D") {
+            Debug.Log(Cantidad * 1075 + " " + "Dolares");
+         }
+        else if (Moneda == "E")
+        {
+            Debug.Log(Cantidad * 1174 + " " + "Euros"); 
+         }
+        else if (Moneda == "R")
+        {
+            Debug.Log(Cantidad * 225 + " " + "Reales");
+         }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+         }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
