3c62aef842950d156ee054e06daa421d40edd7e3
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2025-04-08 12:03:23 -0300

    Examen 2 terminado

---
+++ Assets/Scripts/Especialidades.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Especialidades : MonoBehaviour
+{
+
+    public string nombreAlumno;
+    public string especialidad;
+    public int año;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Error");
+        }else if (año < 1)
+        {
+            Debug.Log("Error");
+        }
+        else if (año > 5)
+        {
+            Debug.Log("Error");
+        }else if(año<=2 && año >= 1)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico");
+        }else if(especialidad != "T" && especialidad != "G" && especialidad != "M" && especialidad != "D" && especialidad != "H" ){
+            Debug.Log("Solo puede ingresar T, D, G, M o H");
+        }else
+        {
+            Debug.Log("Muchas gracias " + nombreAlumno);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
